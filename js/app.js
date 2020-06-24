97b4c54228bba9be29befd9a4b4964c06ddbe390
CarlossAyala <carlosylautaro@gmail.com>
2020-06-24 12:43:36 -0300

    Usando Bootstrap

---
+++ js/app.js
@@ -0,0 +1,27 @@
+/* Animation */
+window.sr = ScrollReveal();
+sr.reveal(".navbar", {
+  duration: 2000,
+  origin: "bottom",
+});
+sr.reveal(".header-content-left", {
+  duration: 2000,
+  origin: "top",
+  distance: "300px",
+});
+sr.reveal(".header-content-right", {
+  duration: 2000,
+  origin: "bottom",
+  distance: "300px",
+});
+
+/* Smooth Scrolling*/
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
